87c0377e4cb7becefee3fea0e8182ad888a7a95e
glipets <gary.lipets@gmail.com>
2021-07-01 12:42:59 -0600

    firefox update

--- open_youtube.py
+++ open_youtube.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 from selenium import webdriver
-chromedriver = "C:\\Users\\glipets\\Downloads\\chromedriver_win32\chromedriver.exe"
-driver = webdriver.Chrome(chromedriver)
+
+# leave for windows
+#chromedriver = "C:\\Users\\glipets\\Downloads\\chromedriver_win32\chromedriver.exe"
+# leave below for firefox
+#driver= webdriver.Firefox()
+#linux with chromedriver downloaded and CHROME installed
+driver = webdriver.Chrome("/home/jbiden/Downloads/chromedriver")
 driver.get("http://www.youtube.com")
+
+# DO NOT FORGET TO INSTALL THIS
+# sudo pip install webdrivermanager
+# which webdrivermanager
+# sudo webdrivermanager firefox chrome --linkpath /usr/local/bin
+
+# TO INSTALL CHROME on linux
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
